c4c908c3e1cebd0d15b6a5718518f3a1f7546a63
Vladimir Dyagilev <vdyagilev5@gmail.com>
2019-08-08 12:27:43 -0400

    Began creating the NN Classifier. It is a GRU RNN with a Linear layer at the end

--- ChartObjects/ChartView.py
+++ ChartObjects/ChartView.py
@@ -30,3 +30,31 @@ def show_chart(path, symbol, duration):
         yaxis_title='{} Price'.format(symbol))
 
     fig.show()
+
+def show_chart_chunked(path, symbol, duration):
+    hds = HistoricalDataSaver(path)
+    df = hds.load_data()
+
+    # Create a deep copy of df so we can change it without mutation side-effect
+    chart_df = df.copy(deep=True)
+    # change each timestamp into a date
+    for index, entry in chart_df.open_time.items():
+        # we divide by 1000 to remove milliseconds to larger units
+        chart_df.open_time.iloc[index] = datetime.fromtimestamp(entry / 1000)
+    x = len(chart_df)
+    for chart_df in [chart_df[:int(x*0.3)], chart_df[int(x*0.33):int(x*0.66)],
+                     chart_df[int(x*0.66):]]:
+        fig = go.Figure(data=[go.Candlestick(x=chart_df.open_time,
+                                             open=chart_df.open,
+                                             high=chart_df.high,
+                                             low=chart_df.low,
+                                             close=chart_df.close)])
+
+        # Label the chart
+        fig.update_layout(
+            title='Trading View for {} - {}'.format(symbol, duration),
+            yaxis_title='{} Price'.format(symbol))
+
+        fig.show()
+show_chart_chunked('../strategy/training_data.csv', 'ETHBTC', '1h')
+
--- Strategy/TINN.py
+++ Strategy/TINN.py
@@ -1,17 +1,64 @@
-from fastai.tabular import *
-
-class TINN():
-    def __init__(self, data_df, dep_var, categories):
-        procs = [FillMissing, Categorify, Normalize]
-        valid_idx = range(len(data_df) - SPLIT_NUM, len(data_df))
-        cat_names = categories
-
-        self.data = TabularDataBunch.from_df(path, data_df, dep_var, valid_idx=valid_idx, procs=procsm cat_names=cat_names)
-        print(data.train_ds.cont_names)
-
-    def create_learner(self):
-        self.learner = tabular_learner(self.data, layers=[200,100], emb_szs={'native-country':10}, metrics=accuracy)
+import torch
 
 
+class GRUTechNet(torch.nn.Module):
+
+    def __init__(self, num_features, hidden_size, num_layers, output_size):
+        super(GRUTechNet, self).__init__()
+        self.rnn = torch.nn.GRU(num_features, hidden_size, num_layers,
+              dropout=0, bidirectional=False)
+        # Linear layer used to take GRU outputted hidden layer and transform
+        # to buy/hold/sell signal
+        self.lin = torch.nn.Linear(hidden_size, output_size)
+
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+    def forward(self, x):
+        batch_size = x.size(0)
+
+        # Initializing hidden state for first input using method defined below
+        hidden = self.init_hidden(batch_size)
+
+        # Passing in the input and hidden state into the model and obtaining outputs
+        out, hidden = self.rnn(x, hidden)
+
+        # Reshaping the outputs such that it can be fit into the fully connected layer
+        out = out.contiguous().view(-1, self.hidden_size)
+        out = self.lin(out)
+
+        return out, hidden
+
+    def init_hidden(self, batch_size):
+        # This method generates the first hidden state of zeros which we'll use in the forward pass
+        # We'll send the tensor holding the hidden state to the device we specified earlier as well
+        hidden = torch.zeros(self.num_layers, batch_size, self.hidden_size)
+        return hidden
 
 
+# Train model
+model = torch.Model(input_size,output_size,hidden_dim,n_layers)
+# send model to gpu
+model.to(device)
+
+# Define hyperparameters
+n_epochs = 100
+lr = 0.01
+
+# Define loss, Optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+
+# Training
+for epoch in range(1, n_epochs + 1):
+    optimizer.zero_grad()  # Clears existing gradients from previous epoch
+    input_seq.to(device)
+    output, hidden = model(input_seq)
+    loss = criterion(output, target_seq.view(-1).long())
+    loss.backward()  # Does backpropagation and calculates gradients
+    optimizer.step()  # Updates the weights accordingly
+
+    if epoch % 10 == 0:
+        print('Epoch: {}/{}.............'.format(epoch, n_epochs), end=' ')
+        print("Loss: {:.4f}".format(loss.item()))
