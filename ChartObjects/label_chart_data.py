b513682f4e510faa3815aefc3631e23b1f6626a9
Vladimir Dyagilev <vdyagilev5@gmail.com>
2019-08-12 13:38:09 -0400

    downloaded new dataset

--- ChartObjects/label_chart_data.py
+++ ChartObjects/label_chart_data.py
@@ -10,14 +10,8 @@ from strategy.TINNStrategy import prepare_data
 
 operator = BinanceOperator()
 
-data_df = operator.get_historical_candlesticks('ETHBTC', Client.KLINE_INTERVAL_1MINUTE, 'January 19 2019 6:00', 'January 19 2019 6:53')
-tech_data_df = prepare_data(data_df)
-
-tis = ['sma10', 'bb10_low', 'bb10_mid',
-               'bb10_up', 'bb20_low', 'bb20_mid', 'bb20_up']
-
-chart = Charting(df=tech_data_df, col_label='time', row_label='close', tech_inds=tis)
-chart.chart_to_image('chart_image.png')
+data_df = operator.get_historical_candlesticks('ETHBTC', Client.KLINE_INTERVAL_1MINUTE, 'January 1 2017 6:00', 'January 1 2019 6:00')
+data_df.to_csv('ETHBTC-2year-1min')
 
 
 
