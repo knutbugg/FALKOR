49016d18259029cce47712949c48d45e307347e4
Vladimir Dyagilev <vdyagilev5@gmail.com>
2019-08-12 15:20:43 -0400

    created new dataset

--- ChartObjects/label_chart_data.py
+++ ChartObjects/label_chart_data.py
@@ -10,8 +10,8 @@ from strategy.TINNStrategy import prepare_data
 
 operator = BinanceOperator()
 
-data_df = operator.get_historical_candlesticks('ETHBTC', Client.KLINE_INTERVAL_1MINUTE, 'January 1 2017 6:00', 'January 1 2019 6:00')
-data_df.to_csv('ETHBTC-2year-1min')
+data_df = operator.get_historical_candlesticks('ETHBTC', Client.KLINE_INTERVAL_1MINUTE, 'January 1 2015 6:00', 'August 10 2019 6:00')
+data_df.to_csv('ETHBTC-4year-1min')
 
 
 
