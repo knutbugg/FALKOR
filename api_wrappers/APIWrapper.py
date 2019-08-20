6da47a90f465f401b2f32850a674b7a6c05568ac
Vladimir Dyagilev <vdyagilev5@gmail.com>
2019-08-20 14:39:01 -0400

    tabs to spaces

--- api_wrappers/APIWrapper.py
+++ api_wrappers/APIWrapper.py
@@ -1,44 +1,44 @@
 
 
 class APIWrapper:
-	"""Abstract class representing an API Wrapper for a financial security. All required methods must be completed by child class"""
+    """Abstract class representing an API Wrapper for a financial security. All required methods must be completed by child class"""
 
-	def historical_candles(self, symbol: str, interval:str, start_time: str, end_time: str):
-		"""Returns DataFrame with columns = ['time', 'open', 'close', 'high', 'low', 'volume'] for the security. Number of rows is equal to time interval divided by days"""
+    def historical_candles(self, symbol: str, interval:str, start_time: str, end_time: str):
+        """Returns DataFrame with columns = ['time', 'open', 'close', 'high', 'low', 'volume'] for the security. Number of rows is equal to time interval divided by days"""
 
-		raise NotImplementedError
+        raise NotImplementedError
 
-	def last_candles(self, symbol, interval):
-		"""Returns DataFrame containing the most recent candlesticks information"""
+    def last_candles(self, symbol, interval):
+        """Returns DataFrame containing the most recent candlesticks information"""
 
-		raise NotImplementedError
+        raise NotImplementedError
 
-	def live_tickers(self):
-		"""Returns a dictionary containing current ticker info"""
+    def live_tickers(self):
+        """Returns a dictionary containing current ticker info"""
 
-		raise NotImplementedError
+        raise NotImplementedError
 
-	def buy_order(self, amount: int, price="market"):
-		"""Created a buy order for amount. If price != "market", then a buy order will be created at price=price"""
+    def buy_order(self, amount: int, price="market"):
+        """Created a buy order for amount. If price != "market", then a buy order will be created at price=price"""
 
-		raise NotImplementedError
+        raise NotImplementedError
 
-	def sell_order(self, amount:int, price="market"):
-		"""Created a sell order for amount. If price != "market", then a sell order will be created at price=price"""
+    def sell_order(self, amount:int, price="market"):
+        """Created a sell order for amount. If price != "market", then a sell order will be created at price=price"""
 
-		raise NotImplementedError
+        raise NotImplementedError
 
-	def check_balance(self,):
-		"""Returns portfolio balance"""
+    def check_balance(self,):
+        """Returns portfolio balance"""
 
-		raise NotImplementedError
+        raise NotImplementedError
 
-	def trade_status(self, trade_id: str):
-		"""Returns the status for trade with id trade_id"""
+    def trade_status(self, trade_id: str):
+        """Returns the status for trade with id trade_id"""
 
-		raise NotImplementedError
+        raise NotImplementedError
 
-	def cancel_order(self, trade_id: str):
-		"""Cancel order with trade_id"""
+    def cancel_order(self, trade_id: str):
+        """Cancel order with trade_id"""
 
-		raise NotImplementedError
+        raise NotImplementedError
--- models/CNN/CNN.py
+++ models/CNN/CNN.py
@@ -5,37 +5,37 @@ import torchvision.models as models
 
 
 class CNN(nn.Module):
-	def __init__(self):
-		"""Initialize the model by setting up the layers"""
-		super(CNN, self).__init__()
-		
-		# initial layer is resnet
-		self.resnet = models.resnet34(pretrained=True, progress=False)
-		
-		# final fully connected layers
-		self.dense1 = nn.Linear(1000, 500)
-		self.dense2 = nn.Linear(500, 100)
-		self.dense3 = nn.Linear(100, 12)
-		
-		# output layer
-		self.dense4 = nn.Linear(12, 1)
-	
-	def forward(self, x):
-		"""Perform a forward pass of our model on some input and hidden state"""
-		
-		x = self.resnet(x)
-		
-		 # apply three fully-connected Linear layers with ReLU activation function
-		x = self.dense1(x)
-		x = relu(x)
-		
-		x = self.dense2(x)
-		x = relu(x)
-		
-		x = self.dense3(x)
-		x = relu(x)
-		
-		# output is a size 1 Tensor
-		x = self.dense4(x)
-		
-		return x
+    def __init__(self):
+        """Initialize the model by setting up the layers"""
+        super(CNN, self).__init__()
+        
+        # initial layer is resnet
+        self.resnet = models.resnet34(pretrained=True, progress=False)
+        
+        # final fully connected layers
+        self.dense1 = nn.Linear(1000, 500)
+        self.dense2 = nn.Linear(500, 100)
+        self.dense3 = nn.Linear(100, 12)
+        
+        # output layer
+        self.dense4 = nn.Linear(12, 1)
+    
+    def forward(self, x):
+        """Perform a forward pass of our model on some input and hidden state"""
+        
+        x = self.resnet(x)
+        
+         # apply three fully-connected Linear layers with ReLU activation function
+        x = self.dense1(x)
+        x = relu(x)
+        
+        x = self.dense2(x)
+        x = relu(x)
+        
+        x = self.dense3(x)
+        x = relu(x)
+        
+        # output is a size 1 Tensor
+        x = self.dense4(x)
+        
+        return x
